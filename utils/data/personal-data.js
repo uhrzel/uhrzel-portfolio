86d0f3fc7969e62b409191c64dfff095076c17be
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-07-30 10:26:44 +0800

    some change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profileko.jpg',
   designation: 'Self-taught Developer',
   description:
-    'My name is Arzel John Zolina. I am a self taught programmer in my daily life. I am a quick learner with a self-learning attitude. Im currently looking for entry-level position that suits my skills. I prefer to keep learning, I am very passionate about improving my coding skills & developing websites and also in digital marketing. Coding gives me a wonderful feeling of achievement and joy which I cannot explain in words.',
+    'I am Arzel John Zolina, a self-taught programmer passionate about web development and digital marketing. I am a quick learner, always eager to improve my coding skills. Im currently seeking an entry-level position that suits my abilities and drive for continuous learning.',
   email: 'Arzeljrz17@gmail.com',
   phone: '+639090937257',
   address: 'Polomolok South Cotabato',

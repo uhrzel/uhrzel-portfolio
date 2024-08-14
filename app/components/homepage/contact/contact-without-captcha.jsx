faf7fa4f311eb791e46df59388b294ed42b50f11
Arzel <Arzeljrz17@gmail.com>
2024-08-14 09:08:17 +0800

    add more text

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -103,6 +103,7 @@ function ContactWithoutCaptcha() {
               maxLength="500"
               name="message"
               required={true}
+              label="Please include again your email here: "
               onChange={(e) => setInput({ ...input, message: e.target.value })}
               onBlur={checkRequired}
               rows="4"
